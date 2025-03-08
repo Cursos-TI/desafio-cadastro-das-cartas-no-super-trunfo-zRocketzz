76b9c8e19145cd7c2688216b5470b4c4593173dd
zRocketzz <124969601+zRocketzz@users.noreply.github.com>
2025-03-08 17:57:43 +0000

    Desafio finalizado

--- desafio_supertrunfo.c
+++ desafio_supertrunfo.c
@@ -1,45 +1,45 @@
 #include <stdio.h>
 
 // Olá, me chamo Christian Ruan! E aqui é a minha primeira atividade com o professor Sérgio Cardoso.
-// E esse é o desafio nível novato. Criando as Cartas do Super Trunfo!
+// E esse é o desafio nível novato de Cartas Super Trunfo. Criando as Cartas do Super Trunfo!
 
 int main() {
+                                               //Declando as variaveis
 
-    char estado;              // Representa o Estado, por exemplo: A, B, C, D...
-    char codigo[4];           // Representa o Código da carta, por exemplo: A01, B02, C03, D04...
-    char cidade[50];          // Para armazenar e representar o nome do Estado utilizado.
-    int populacao;            // Número da população.
-    double area;              // Número da área por km².
-    double pib;               // Referente ao número do PIB.
-    int ponto;                // Quantos Pontos Turísticos existem nesse local.
+    char estado, estado2;                    // Representa o Estado, por exemplo: A, B, C, D...
+    char codigo[50], codigo2[50];           // Representa o Código da carta, por exemplo: A01, B02, C03, D04...
+    char cidade[50], cidade2[50];          // Para armazenar e representar o nome do Estado utilizado.
+    int populacao, populacao2;            // Número da população.
+    double area, area2;                  // Número da área por km².
+    double pib, pib2;                   // Referente ao número do PIB.
+    int ponto, ponto2;                 // Quantos Pontos Turísticos existem nesse local.
 
     printf("Olá, seja bem-vindo ao Desafio do Super Trunfo! \n");
     printf("Utilizaremos 2 cartas para fazer o jogo. \n");
 
+                                   //Função printf exibe na tela e scanf coleta as informações referentes a carta 02
     printf("Carta número 1: \n");
-    getchar();  // Limpa o Enter do buffer
-
     printf("Digite uma letra de A a D: \n");
-    scanf("%c", &estado);
-    getchar();  // Limpa o Enter do buffer para evitar problemas na leitura seguinte
+    scanf(" %c", &estado);
+    getchar();                    // Limpa o Enter do buffer para evitar problemas na leitura seguinte
 
     printf("Digite o código da carta: \n");
     scanf("%s", codigo);
 
-    printf("Digite qual será o Estado: \n");
-    getchar();  // Para limpar o buffer antes de fgets()
+    printf("Estado: \n");
+    getchar();                   // Para limpar o buffer antes de fgets()
     fgets(cidade, 50, stdin);
 
-    printf("Digite qual é o número da população: \n");
+    printf("População: \n");
     scanf("%d", &populacao);
 
-    printf("Digite o valor da área: \n");
+    printf("Área: \n");
     scanf("%lf", &area);
 
-    printf("Digite o PIB: \n");
+    printf("PIB: \n");
     scanf("%lf", &pib);
 
-    printf("Quantos pontos turísticos existem nesse local? \n");
+    printf("Pontos turísticos: \n");
     scanf("%d", &ponto);
 
     printf("Finalizamos os dados da primeira carta! \n");
@@ -53,29 +53,53 @@ int main() {
     getchar();
 
     printf("Digite uma letra de A a D: \n");
-    scanf("%c", &estado);
+    scanf(" %c", &estado2);
     getchar();  // Limpa o Enter do buffer para evitar problemas na leitura seguinte
 
     printf("Digite o código da carta: \n");
-    scanf("%s", codigo);
+    scanf("%s", codigo2);
 
-    printf("Digite qual será o Estado: \n");
+    printf("Estado: \n");
     getchar();  // Para limpar o buffer antes de fgets()
-    fgets(cidade, 50, stdin);
+    fgets(cidade2, 50, stdin);
 
-    printf("Digite qual é o número da população: \n");
-    scanf("%d", &populacao);
+    printf("População: \n");
+    scanf("%d", &populacao2);
 
     printf("Digite o valor da área: \n");
-    scanf("%lf", &area);
+    scanf("%lf", &area2);
 
     printf("Digite o PIB: \n");
-    scanf("%lf", &pib);
+    scanf("%lf", &pib2);
 
-    printf("Quantos pontos turísticos existem nesse local? \n");
-    scanf("%d", &ponto);
+    printf("Pontos turísticos: \n");
+    scanf("%d", &ponto2);
 
     //Agora finalizando, vamos fazer aparecer as duas cartas.
-    printf("Carta: ")
+    //Printf para exibir na tela todas as informações digitadas pelo usuário referente a carta 01 e 02.
+
+    printf("Carta 1: ");
+    printf("Nome do Estado: %c\n", estado);
+    printf("Código da carta: %s\n",codigo);
+    printf("Nome da cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %lf km²\n", area);
+    printf("PIB: %.2lf bilhões\n", pib);                // Formatado para 2 casas decimais
+    printf("Números de Pontos Turísticos: %d\n", ponto); 
+
+    //Agora vamos fazer aparecer a segunda carta.
+
+    printf("Carta 2:"); 
+    printf("Nome do Estado: %c\n", estado2);
+    printf("Código da carta: %s\n",codigo2);
+    printf("Nome da cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %lf km²\n", area2);
+    printf("PIB: %.2lf bilhões\n", pib2);                // Formatado para 2 casas decimais
+    printf("Números de Pontos Turísticos: %d\n", ponto2);
+    
+    printf("Desafio finalizado! Obrigado pela sua colaboração!\n");
+
+
     return 0;
 }
